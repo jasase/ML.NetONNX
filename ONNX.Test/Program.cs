25d8cdfbd7a618c360636f8ad551d80061598a79
Sternheim, Sascha (sternheims) <sascha.sternheim@conti.de>
2020-01-15 14:48:57 +0100

    Projektdateien hinzufügen.

---
+++ ONNX.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ONNX.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
